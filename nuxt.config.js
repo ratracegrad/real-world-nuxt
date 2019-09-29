b08068b6cc8b5c54c46acfefea34e33ea3220abf
Jennifer Bland <ratracegrad@gmail.com>
2019-09-29 07:45:39 -0400

    netlify deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+import EventService from './services/EventService';
 export default {
     mode: 'universal',
     /*
@@ -52,5 +53,14 @@ export default {
          ** You can extend webpack config here
          */
         extend(config, ctx) {}
+    },
+    generate: {
+        routes: () => {
+            return EventService.getEvents().then(response => {
+                return response.data.map(event => {
+                    return `/event/${event.id}`;
+                });
+            });
+        }
     }
 };
