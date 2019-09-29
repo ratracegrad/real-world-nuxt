f50a71873a02684aace04fdbb09641cc0460dd37
Jennifer Bland <ratracegrad@gmail.com>
2019-09-29 07:32:56 -0400

    heroku deployment

---
+++ pages/event/create.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Create an Event</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Create',
+    head() {
+        return {
+            title: 'Create an Event',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'You can create a new event in your neighborhood'
+                }
+            ]
+        };
+    }
+};
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,91 @@
 <template>
-  <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        real-world-nuxt
-      </h1>
-      <h2 class="subtitle">
-        My polished Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-      </div>
+        <h1>Events</h1>
+        <EventCard
+            v-for="(event, $eventIndex) in events"
+            :key="$eventIndex"
+            :event="event"
+            :data-index="$eventIndex"
+        ></EventCard>
     </div>
-  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import { mapState } from 'vuex';
+import EventCard from '@/components/EventCard.vue';
 
 export default {
-  components: {
-    Logo
-  }
-}
+    head() {
+        return {
+            title: 'Event Listing'
+        };
+    },
+    components: {
+        EventCard
+    },
+    computed: mapState({
+        events: state => state.events.events
+    }),
+    // asyncData({ $axios, error }) {
+    //     return $axios
+    //         .get('http://localhost:3000/events')
+    //         .then(response => {
+    //             return {
+    //                 events: response.data
+    //             };
+    //         })
+    //         .catch(e => {
+    //             error({
+    //                 statusCode: 503,
+    //                 message:
+    //                     'Unable to fetch events at this time. Please try again.'
+    //             });
+    //         });
+    // }
+    async fetch({ store, error }) {
+        try {
+            await store.dispatch('events/fetchEvents');
+        } catch (err) {
+            error({
+                statusCode: 503,
+                message:
+                    'Unable to fetch events at this time. Please try again.'
+            });
+        }
+    }
+};
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
+        BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+        sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
 }
 
 .subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
 }
 
 .links {
-  padding-top: 15px;
+    padding-top: 15px;
 }
 </style>
